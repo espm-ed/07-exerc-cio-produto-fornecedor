41080d06260cd7e2c15b38db8bec3f1d8eaf7a6b
Antonio Marcos Selmini <marcos.selmini@uol.com.br>
2025-03-28 08:20:51 -0300

    menu principal da classe app

--- src/App.java
+++ src/App.java
@@ -1,3 +1,6 @@
+import static javax.swing.JOptionPane.*;
+import static java.lang.Integer.parseInt;
+import static java.lang.Double.parseDouble;
 public class App {
     
     ListaDupla<Produto> listaProduto = new ListaDupla<>();
@@ -5,7 +8,24 @@ public class App {
 
     public static void main(String[] args) throws Exception {
         
-        
+        menuPrincipal();
 
     }
+
+    public static void menuPrincipal() {
+        String menu = "1. Cadastrar produto\n2. Pesquisar produto\n3. Pesquisar        fornecedor\n4. Finalizar";
+        int opcao;
+        int resposta;
+
+        while(true) {
+            opcao = parseInt(showInputDialog(menu));
+            if(opcao == 4) {
+                resposta = showConfirmDialog(null, "Tem certeza que deseja sair?");
+                if(resposta == YES_OPTION) {
+                    break;
+                }
+            }
+        }
+    }
+
 }
